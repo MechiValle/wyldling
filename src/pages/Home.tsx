e02374a97628a3143a44ee7a133647f8bb023745
Mercedes <Mercedesvalle17@gmail.com>
2026-09-16 02:32:19 -0300

    Added disclaimer about export/import data in Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,6 +6,10 @@ export function Home() {
       <p className="text-sm leading-relaxed">
         This is a project with every little guide we would've loved to have while we were completing Wylde Flowers. In here, you'll find information about relationships, favorite gifts, where to catch fish, where to find recipes, and much more! You can also track your progress, if you want. No personal info required, no ads, just a small free tool to help you complete the game.
       </p>
+      <p className="text-sm leading-relaxed">
+        Disclaimer: all the information is stored locally in your browser. If you want to save it in order to be able to clear or switch browsers, please check our Settings page to export a copy of your data.
+      </p>
+      
         <p className="text-sm leading-relaxed">
         This site is under continuous improvement and expansion, we're more than glad to receive any suggestions for future implementation.
       </p>
